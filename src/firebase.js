cf366c37d504688349ea79a944fef2ed96d126d4
david <daviddecastro42@live.co.uk>
2021-01-20 00:34:14 +0000

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCtaj_km3sy00Ooap07mtji-FG_6w5GNmQ',
+  authDomain: 'ecomerce-react-app.firebaseapp.com',
+  projectId: 'ecomerce-react-app',
+  storageBucket: 'ecomerce-react-app.appspot.com',
+  messagingSenderId: '528955151195',
+  appId: '1:528955151195:web:0c3a5c9b2c1e158beec07e',
+  measurementId: 'G-T3MW0NLB2C'
+};
